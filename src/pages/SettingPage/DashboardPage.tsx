4babddad9ad76f2959c5e7042d55569e2e54fe21
bukhaled2002 <Bakhaled.310@gmail.com>
2025-08-16 22:05:08 +0300

    add button to view brand

--- src/pages/SettingPage/DashboardPage.tsx
+++ src/pages/SettingPage/DashboardPage.tsx
@@ -5,15 +5,19 @@ import { useOffers } from "@/hooks/useOffers";
 import StatsCards from "@/components/StatsCard";
 import { useStats } from "@/hooks/useStats";
 import { useAuth } from "@/context/useAuth";
+import { Link } from "react-router-dom";
 
 export default function DashboardPage() {
   const { brands } = useAuth();
-  const brandId = brands[0]?.id;
+  const brand = brands[0];
+  const brandId = brand?.id;
+
   const {
     deals,
     isLoading: isLoadingDeals,
     error: isErrorDeals,
   } = useOffers({ myOffers: true, perPage: 50 });
+
   const {
     analytics,
     clicks,
@@ -23,11 +27,19 @@ export default function DashboardPage() {
     viewsError,
     viewsLoading,
   } = useStats(brandId);
-  console.log(viewsError);
-  console.log(viewsLoading);
-  console.log(clicks);
+  console.log(viewsError, viewsLoading);
   return (
     <main className="py-6 space-y-8">
+      {/* Button to view brand template */}
+      {brandId && (
+        <div className="mt-6">
+          <Link to={`/brands/${brandId}`} className="">
+            <button className="px-4 py-2 bg-primary cursor-pointer text-white rounded hover:bg-primary/90">
+              View Brand
+            </button>
+          </Link>
+        </div>
+      )}
       {/* Analytics number */}
       {analyticsLoading && <div>Loading stats...</div>}
       {analyticsError && <div>Failed to load stats</div>}
@@ -43,9 +55,9 @@ export default function DashboardPage() {
       {/* Charts */}
       <div className="flex items-center gap-10 flex-col md:flex-row">
         <ViewsChart views={views} />
-        {/* <ChartBarClick /> */}
         <ChartBarClick days={clicks} />
       </div>
+
       {/* Active brand deals */}
       {isLoadingDeals && <div>Loading deals...</div>}
       {isErrorDeals && <div>Failed to load deals</div>}
